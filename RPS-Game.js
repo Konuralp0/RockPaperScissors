68a863abd2e357152c10690c9afd3e2399aec18b
Konuralp <konuralpcelikyay@gmail.com>
2023-02-05 22:50:31 +0100

    Design Improvements

--- RPS-Game.js
+++ RPS-Game.js
@@ -1,6 +1,7 @@
 const rockButton = document.getElementById("rock");
 const paperButton = document.getElementById("paper");
 const scissorsButton = document.getElementById("scissors");
+const inf = document.getElementById("inf");
 let playerEl = document.getElementById("player-el");
 let computerEl = document.getElementById("computer-el");
 const choices = ["r","p","s"];
@@ -8,10 +9,31 @@ let humanChoice;
 let humanPoint = 0;
 let cpmtPoint = 0;
 
+let messagesWin = {
+    rock: "You Won!!  Rock beats scissor",
+    paper:"You Won!!  Paper beats rock",
+    scissor: "You Won!!  Scissor beats paper",
+    
+}
+
+let messagesLose = {
+    rock: "You Lost :( Paper beats rock",
+    paper: "You Lost :( Scissor beats paper",
+    scissors: "You Lost :( Rock beats scissor"
+}
+
+
+
+function changeBackground(element,color){
+    document.getElementById(element).style.backgroundColor = color;
+ 
+}
+
 function rock(){
     humanChoice ="r";
     console.log(humanChoice);
-    playSingleRound();
+    playSingleRound("rock");
+    
     
 }
 function paper(){
@@ -29,25 +51,48 @@ function scissors(){
 
 
 
+
+
+
 function getComputerChoice(){
     return choices[Math.floor(Math.random()* choices.length)];
 }
 
 
-function playSingleRound(){
+function playSingleRound(element){
     let computerChoice = getComputerChoice();
     console.log(humanChoice+computerChoice)
     switch(humanChoice+computerChoice){
-        case "rs":
-        case "pr":
-        case "sp":
-             humanPoint++;
+        case "rs": inf.textContent = messagesWin.rock;
+            humanPoint++;
              playerEl.textContent = humanPoint;
              computerEl.textContent = cpmtPoint;
+             break;
+        case "pr": inf.textContent = messagesWin.paper;
+        humanPoint++;
+             playerEl.textContent = humanPoint;
+             computerEl.textContent = cpmtPoint;
+             break;
+        case "sp": inf.textContent = messagesWin.scissor;
+            humanPoint++;
+             playerEl.textContent = humanPoint;
+             computerEl.textContent = cpmtPoint;
+             break;
+             
+             
+             
+            
+        case "rp": inf.textContent = messagesLose.rock;
+        cpmtPoint++;
+            playerEl.textContent = humanPoint;
+            computerEl.textContent = cpmtPoint;
             break;
-        case "rp":
-        case "ps":
-        case "sr":
+        case "ps": inf.textContent = messagesLose.paper;
+        cpmtPoint++;
+            playerEl.textContent = humanPoint;
+            computerEl.textContent = cpmtPoint;
+            break;
+        case "sr":inf.textContent = messagesLose.scissors;
             cpmtPoint++;
             playerEl.textContent = humanPoint;
             computerEl.textContent = cpmtPoint;
@@ -55,10 +100,15 @@ function playSingleRound(){
         case "rr":
         case "pp":
         case "ss":
-            return"Its a draw";
+            inf.textContent  = " It's a draw!!! "
             break;
     }
+
+
+    
 }
+
+
 
 
 
